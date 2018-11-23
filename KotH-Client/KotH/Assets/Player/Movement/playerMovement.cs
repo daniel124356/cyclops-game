79648e140ed4e815cc51ae84461af33b72cc4a15
JasGamesAU <jasoking2@gmail.com>
2018-11-23 22:23:15 +0800

    Character

---
+++ KotH-Client/KotH/Assets/Player/Movement/playerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public float speed = 18;
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + movement);
+	}
+}
