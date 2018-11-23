9322e09097bbfb4d8865f89687d3b5af75f05961
JasGamesAU <jasoking2@gmail.com>
2018-11-24 00:08:41 +0800

    Network 2

---
+++ KotH-Client/KotH/Assets/Scripts/Player/Movement/playerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class playerMovement : NetworkBehaviour {
+
+    public float speed = 18;
+
+    public Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + movement);
+	}
+}
