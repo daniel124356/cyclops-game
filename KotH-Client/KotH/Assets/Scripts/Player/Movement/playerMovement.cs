74a356d1835e2afd71be8b18f516483f1988a986
JasGamesAU <jasoking2@gmail.com>
2018-11-24 14:13:19 +0800

    vbc

--- KotH-Client/KotH/Assets/Scripts/Player/Movement/playerMovement.cs
+++ KotH-Client/KotH/Assets/Scripts/Player/Movement/playerMovement.cs
@@ -8,10 +8,17 @@ public class playerMovement : NetworkBehaviour {
     public float speed = 1000;
 
     public Rigidbody rb;
+    public float test = 0.1f;
 
 
-	// Use this for initialization
-	void Start () {
+    [SyncVar]
+    Vector3 realPosition = Vector3.zero;
+    [SyncVar]
+    Quaternion realRotation;
+    private float updateInterval;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -20,16 +27,33 @@ public class playerMovement : NetworkBehaviour {
 
 
 
-        if (!isLocalPlayer)
-        {
-            return;
-        }
-
         float hAxis = Input.GetAxis("Horizontal");
         float vAxis = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(hAxis, 0, vAxis) * speed * Time.deltaTime;
 
         rb.MovePosition(transform.position + movement);
+
+
+        if(isLocalPlayer)
+        {
+            updateInterval += Time.deltaTime;
+            if (updateInterval > 0.11f)
+            {
+                updateInterval = 0;
+                CmdSync(transform.position, transform.rotation);
+            }
+        } else
+        {
+            transform.position = Vector3.Lerp(transform.position, realPosition, test);
+            transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, test);
+            //return;
+        }
 	}
+    [Command]
+    void CmdSync(Vector3 position, Quaternion rotation)
+    {
+        realPosition = position;
+        realRotation = rotation;
+    }
 }
