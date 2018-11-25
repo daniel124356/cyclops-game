dc10298c2f5bd2c051dec81b8b43f7fe47d00725
JasGamesAU <jasoking2@gmail.com>
2018-11-25 23:00:30 +0800

    Menu update

---
+++ KotH-Client/KotH/Assets/Scripts/Menu/menuHoverSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class menuHoverSound : MonoBehaviour, IPointerEnterHandler {
+
+    public AudioSource sounds;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Mouse is over text");
+        sounds.Play(0);
+    }
+}
---
+++ KotH-Client/KotH/Assets/Scripts/Menu/menuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour {
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);
+    }
+}
