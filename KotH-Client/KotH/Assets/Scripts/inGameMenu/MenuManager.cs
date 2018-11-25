4fc6b19cb9d8e1689511571a6c321c11e96ac3e7
Daniel <daniel2001john@gmail.com>
2018-11-25 23:44:55 +0800

    escape now quits the game but need to change it to an in-game options menu

---
+++ KotH-Client/KotH/Assets/Scripts/inGameMenu/MenuManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+/*	public OptionsMenu optionsMenu;
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			optionsMenu.gameObject.SetActive(!optionsMenu.gameObject.activeSelf);
+		}
+	} */
+}
