2c3780b8b03c674a2b53065d4d86bcbb47fcde86
Daniel <daniel2001john@gmail.com>
2018-11-25 17:00:45 +0800

    Start of the main menu

---
+++ KotH-Client/KotH/Assets/Scripts/Level Loader/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+
+	public GameObject loadingScreen;
+	public Slider slider;
+
+	public void loadLevel (int sceneIndex)
+	{
+		StartCoroutine(LoadAsynchronously(sceneIndex));
+	}
+
+	IEnumerator LoadAsynchronously (int sceneIndex)
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+		loadingScreen.SetActive(true);
+
+		while (!operation.isDone)
+		{
+			float progress = Mathf.Clamp01(operation.progress / .09f);
+			
+			slider.value = progress;
+
+			yield return null;
+		}
+	}
+
+}
