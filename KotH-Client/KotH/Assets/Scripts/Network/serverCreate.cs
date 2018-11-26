24a548acf8c6f77ae226ecaf49ef1088a548f0b4
JasGamesAU <jasoking2@gmail.com>
2018-11-26 20:07:12 +0800

    DXFGHDHFG

---
+++ KotH-Client/KotH/Assets/Scripts/Network/serverCreate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class serverCreate : MonoBehaviour {
+
+    [SerializeField]
+    private uint roomSize = 5;
+    private string roomNameDefault = "OfficalServer - King Of The Kill AU #";
+
+    private string roomName;
+
+    private NetworkManager networkManager;
+
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if(networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+    }
+
+    public void SetRoomName(string _name)
+    {
+        roomName = _name;
+    }
+
+    public void CreateRoom()
+    {
+        roomName = roomNameDefault + Random.Range(1,20);
+        Debug.Log("Creating Server: " + roomName + " Max Players: " + roomSize);
+        networkManager.matchMaker.CreateMatch(roomNameDefault, roomSize, true, "", "", "", 0, 1, networkManager.OnMatchCreate);
+    }
+}
